14be9ff8a44cfe914681b5303d683d5ec6c32061
Zaria7 <100790950+Zaria7@users.noreply.github.com>
2022-06-05 16:44:49 +0300

    Add files via upload

---
+++ Task 9/script.js
@@ -0,0 +1,29 @@
+/* ------------------------------ TASK 9 ---------------------------------------------------------------
+Sukurkite konstruktoriaus funkciją "Movie" (naudokte ES6), kuri sukuria objektus 3 savybėm ir 1 metodui:
+
+Savybės:
+title, director, budget
+Metodas: 
+wasExpensive() - jeigu filmo budget bus didesnę nei 100 000 000 mln USD, tada gražins true, kiru atveju false 
+------------------------------------------------------------------------------------------------------ */
+
+class Movie {
+    constructor(title, director, budget) {
+        this.title=title;
+        this.director=director;
+        this.budget=budget;
+    }
+
+wasExpensive = function () {
+    if(this.budget>100000000) {
+        return true
+    } else{
+        return false
+    }
+    }
+}
+
+
+const titanikas = new Movie("Titanic", " James Cameron", 200000000)
+
+console.log(titanikas.wasExpensive())
