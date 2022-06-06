84c67efbe1fbc11e7c5c0ddce025db87a2de8717
Zaria7 <100790950+Zaria7@users.noreply.github.com>
2022-06-06 15:07:36 +0300

    Add files via upload

---
+++ Task 6 pagerinta/script.js
@@ -0,0 +1,46 @@
+/* ------------------------------ TASK 6 -----------------------------------
+Turimas "users" masyvas. 
+
+Parašykite funckijas, kurios atliks nurodytas užduotis
+1. funkcija "getUserAverageAge" - kaip argumentą priims masyvą ir duoto masyvo 
+atveju grąžins visų "users" amžiaus vidurkį kaip skaičių.
+2. funkcija "getUsersNames" -  kaip argumentą priims masyvą ir duoto masyvo 
+atveju grąžins visų "users" vardus naujame masyve pvz., ['John Smith', 'Ann Smith'..].
+-------------------------------------------------------------------------- */
+
+const users = [
+  { id: '1', name: 'John Smith', age: 20 },
+  { id: '2', name: 'Ann Smith', age: 24 },
+  { id: '3', name: 'Tom Jones', age: 31 },
+  { id: '4', name: 'Rose Peterson', age: 17 },
+  { id: '5', name: 'Alex John', age: 25 },
+  { id: '6', name: 'Ronald Jones', age: 63 },
+  { id: '7', name: 'Elton Smith', age: 16 },
+  { id: '8', name: 'Simon Peterson', age: 30 },
+  { id: '9', name: 'Daniel Cane', age: 51 },
+];
+
+/*1. funkcija "getUserAverageAge" - kaip argumentą priims masyvą ir duoto masyvo 
+atveju grąžins visų "users" amžiaus vidurkį kaip skaičių.*/
+
+
+function getUserAverageAge (array) {
+  const ages = array.map((user) => user.age);
+  let sum = 0;
+  ages.forEach((value) =>{
+    sum +=value;
+  })
+  const avg = Math.round(sum/ages.length);
+  return avg;
+}
+
+console.log(getUserAverageAge(users))
+
+/*2. funkcija "getUsersNames" -  kaip argumentą priims masyvą ir duoto masyvo 
+atveju grąžins visų "users" vardus naujame masyve pvz., ['John Smith', 'Ann Smith'..].*/
+
+
+const getUsersNames = (array) =>
+array.map((user) => `${user.name}`);
+
+console.log(getUsersNames(users))
